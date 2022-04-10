af2d7d08ff3f4d9e12cc370ab6d7cd80b3f891f7
Frankee.zhou <frankee.zhou@wayz.ai>
2022-04-10 17:02:02 +0800

    1. update the the JSONDbDataType implement 2. add db key attribute

--- go/go.mod
+++ go/go.mod
@@ -4,11 +4,11 @@ go 1.16
 
 require (
 	github.com/json-iterator/go v1.1.12
-	github.com/mojo-lang/core/go v0.0.0-20220404130715-b693482c3a6a
+	github.com/mojo-lang/core/go v0.0.0-20220410082624-b8a6b94b78be
 	github.com/mojo-lang/lang/go v0.0.0-20220404120718-946d0e1214df
 	github.com/stretchr/testify v1.7.0
 	golang.org/x/sys v0.0.0-20210615035016-665e8c7367d1 // indirect
 	google.golang.org/protobuf v1.28.0
-	gorm.io/driver/sqlite v1.2.6
-	gorm.io/gorm v1.23.2
+	gorm.io/driver/sqlite v1.3.1
+	gorm.io/gorm v1.23.4
 )
--- go/pkg/mojo/db/json.go
+++ go/pkg/mojo/db/json.go
@@ -1,57 +1,47 @@
 package db
 
 import (
-	"database/sql/driver"
-	"fmt"
-	jsoniter "github.com/json-iterator/go"
-	"gorm.io/gorm"
-	"gorm.io/gorm/schema"
-	"reflect"
+    "database/sql/driver"
+    "fmt"
+    jsoniter "github.com/json-iterator/go"
+    "github.com/mojo-lang/core/go/pkg/mojo/core"
+    "gorm.io/gorm"
+    "gorm.io/gorm/schema"
+    "reflect"
 )
 
 type JSONValuer struct{}
 
 func (v JSONValuer) Value(value interface{}) (driver.Value, error) {
-	if val := reflect.ValueOf(value); val.IsZero() {
-		return nil, nil
-	}
+    if val := reflect.ValueOf(value); val.IsZero() {
+        return nil, nil
+    }
 
-	return jsoniter.ConfigFastest.MarshalToString(value)
+    return jsoniter.ConfigFastest.MarshalToString(value)
 }
 
 type JSONScanner struct{}
 
 func (s JSONScanner) Scan(value interface{}, src interface{}) error {
-	if src == nil {
-		return nil
-	}
-	if val := reflect.ValueOf(value); !val.IsValid() || (val.CanAddr() && val.IsNil()) {
-		return nil
-	}
+    if src == nil {
+        return nil
+    }
+    if val := reflect.ValueOf(value); !val.IsValid() || (val.CanAddr() && val.IsNil()) {
+        return nil
+    }
 
-	var bytes []byte
-	switch v := src.(type) {
-	case []byte:
-		bytes = v
-	case string:
-		bytes = []byte(v)
-	default:
-		return fmt.Errorf("failed to unmarshal JSONB value: %v", src)
-	}
-
-	return jsoniter.ConfigFastest.Unmarshal(bytes, value)
+    switch v := src.(type) {
+    case []byte:
+        return jsoniter.ConfigFastest.Unmarshal(v, value)
+    case string:
+        return jsoniter.ConfigFastest.UnmarshalFromString(v, value)
+    default:
+        return fmt.Errorf("failed to unmarshal JSONB value: %v", src)
+    }
 }
 
 type JSONDbDataType struct{}
 
 func (JSONDbDataType) GormDBDataType(db *gorm.DB, field *schema.Field) string {
-	switch db.Dialector.Name() {
-	case "sqlite":
-		return "JSON"
-	case "mysql":
-		return "JSON"
-	case "postgres":
-		return "JSONB"
-	}
-	return ""
+    return core.GormDBJSONType(db)
 }
